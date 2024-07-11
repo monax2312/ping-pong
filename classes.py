035bcb961cb65353b4b29a3fb9fad4474196cfb1
monax2312 <artemd.pleshko@gmail.com>
2024-07-11 10:35:58 +0300

    init

--- classes.py
+++ classes.py
@@ -2,17 +2,53 @@ import pygame
 
 pygame.init()
 
-class GameSprite(sprite.Sprite):
-    def __init__(self, p_image: str, x: int, y: int, w: int, h: int, speed: int):
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, p_image: str, x: int, y: int, w: int, h: int):
         super().__init__()
-        self.image = transform.scale(image.load(p_image), (w, h))
+        self.image = pygame.transform.scale(pygame.image.load(p_image), (w, h))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
 class Ball(GameSprite):
-    ...
+    def __init__(self, dx: int, dy: int, p_image: str, x: int, y: int, w: int, h: int):
+        super().__init__(p_image, x, y, w, h)
+        self.dx = dx
+        self.dy = dy
+    def update(self, d, rocket1, rocket2):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+        if self.rect.y > 450:
+            self.dy *= -1
+        if self.rect.y < 0:
+            self.dy *= -1
+        if self.rect.colliderect(rocket1.rect):
+            self.dx *= -1
+        if self.rect.colliderect(rocket2.rect):
+            self.dx *= -1
+
+        d.blit(self.image, self.rect)
+        
+
 
 class Player(GameSprite):
-    ...
+    def __init__(self, up:str, down: str, x: int, y: int, w: int, h: int, speed: int):
+        self.image = pygame.Surface((w, h))
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+        self.down = down
+        self.up = up
+    def update(self, d):
+        key = pygame.key.get_pressed()
+
+        if key[self.up] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if key[self.down] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+        d.blit(self.image, self.rect)
 
--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randint
+from random import randint, choice
 from classes import *
 
 pygame.init()
@@ -8,25 +8,57 @@ dis = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('пинг понг')
 
 
-back = (randint(0, 255), randint(0, 255), randint(0, 255))
+back = (randint(50, 255), randint(50, 255), randint(50, 255))
 
 clock = pygame.time.Clock()
 
 game = True
 finish = False
 
+score1 = 0
+score2 = 0
 
-rocket1 = Player(pygame.Surface(), 30, 200, 50, 150)
-rocket2 = Player(pygame.Surface(), 520, 200, 50, 150)
-ball = Ball('pngwing.com.png', 200, 200, 50, 50)
+font = pygame.font.SysFont('comicsans', 40)
+
+
+rocket1 = Player(pygame.K_w, pygame.K_s, 30, 200, 50, 150, 7)
+rocket2 = Player(pygame.K_UP, pygame.K_DOWN,  720, 200, 50, 150, 7)
+ball = Ball(choice([-6, 6]), choice([-6, 6]), 'pngwing.com.png', 400, 200, 50, 50)
 
 while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
+            finish = False
+            rocket1.rect.y = 200
+            rocket2.rect.y = 200
+            ball = Ball(choice([-6, 6]), choice([-6, 6]), 'pngwing.com.png', 400, 200, 50, 50)
+
 
     if not finish:
         dis.fill(back)
+
+        rocket1.update(dis)
+        rocket2.update(dis)
+        ball.update(dis, rocket1, rocket2)
+
+        score_text = f'{score1} / {score2}'
+        score_text_b = font.render(score_text, 1, (0,0,0))
+        dis.blit(score_text_b, (380, 20))
+
+        if ball.rect.x < 0:
+            finish = True
+            score2 += 1
+        if ball.rect.x > 750:
+            finish = True
+            score1 += 1
+    else:
+        text = f'press space to restart'
+        text_b = font.render(text, 1, (0,0,0))
+        dis.blit(text_b, (280, 240))
+
+
 
 
     
